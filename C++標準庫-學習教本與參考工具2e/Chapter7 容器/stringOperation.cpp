4c15d8f54134ff22ffc753cb185406a641ce4ac5
unknown <blue1118.ss@gmail.com>
2021-09-12 12:10:26 +0800

    Upload Chapter07 Container notes

---
+++ C++標準庫-學習教本與參考工具2e/Chapter7 容器/stringOperation.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string inputString("Hello C++ Language");
+    string str("C++");
+    //find(string str):返回第一個找到的字串 第一個字元索引值
+    int pos = inputString.find("C++");
+    //檢查是否存在
+    if (pos != inputString.npos) {
+        //erase(int pos,int num):刪除 從str.at(pos)開始算num個字元
+        inputString.erase(pos, 3);
+    }
+    cout << inputString << endl;  //Hello  Language
+    return 0;
+}
